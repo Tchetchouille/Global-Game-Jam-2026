c5aa3e50c5cc176a72ee41628f89578b5434f061
Jeremias Kuehne <jeremias.kuehne@protonmail.com>
2026-01-31 16:47:54 +0100

    added tile colors

---
+++ game-jam-basis/Jeremias/Level/tiles.gd
@@ -0,0 +1,19 @@
+extends TileMapLayer
+
+@onready var rgb = $"../.."
+var modulation = [
+	Vector3(0.6, 0.07, 0.3),
+	Vector3(0.3, 0.6, 0.07),
+	Vector3(0.07, 0.3, 0.6)
+]
+@export var layer : int
+
+func _process(delta: float) -> void:
+	if rgb.mask == layer:
+		modulate.a = 0.2
+	else:
+		modulate.a = 1
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	modulate = Color(modulation[layer].x, modulation[layer].y, modulation[layer].z)
