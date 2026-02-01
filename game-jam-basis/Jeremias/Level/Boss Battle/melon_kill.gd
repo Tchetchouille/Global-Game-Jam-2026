0ca778fbc636a79f60302f6966b51f3ca81cc3f9
ColinLug <colin.lugin@gmail.com>
2026-02-01 16:04:51 +0100

    killzone_melon

---
+++ game-jam-basis/Jeremias/Level/Boss Battle/melon_kill.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+@onready var timer: Timer = $Timer
+func _on_body_entered(body: Node2D) -> void:
+	if body.has_method("die"):
+		body.die()
+	# Brute force check de si le body c'est le player
+	if body.has_method("change_mask"):
+		print("You died")
+		Engine.time_scale = 0.7
+		timer.start()
+
+
+func _on_timer_timeout() -> void:
+	Engine.time_scale = 1.0
+	get_tree().reload_current_scene() # Replace with function body.
