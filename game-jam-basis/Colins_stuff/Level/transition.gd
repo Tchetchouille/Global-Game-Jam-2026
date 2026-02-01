d4b216a5709f10b42919b4a5223ea72b5b6db344
ColinLug <colin.lugin@gmail.com>
2026-02-01 15:55:59 +0100

    melon kill Luigi

--- game-jam-basis/Colins_stuff/Level/transition.gd
+++ game-jam-basis/Colins_stuff/Level/transition.gd
@@ -2,4 +2,11 @@ extends Area2D
 
 @onready var color_rect = $"../CanvasLayer/ColorRect"
 func _on_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
+	var tween = get_tree().create_tween()
+	tween.tween_property(color_rect, "color", Color(0,0,0,1),0.5)
+	$"Timer".start()
+	
+
+
+func _on_timer_timeout() -> void:
+	get_tree().change_scene_to_file("res://Jeremias/Level/Boss Battle/boss_battle.tscn")
